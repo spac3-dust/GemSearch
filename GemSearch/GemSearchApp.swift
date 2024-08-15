04628a54e4928d175eca5e5bfd08e66d447315ce
Abe <abepark013@gmail.com>
2024-08-15 15:57:35 -0400

    Initial Commit

---
+++ GemSearch/GemSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GemSearchApp.swift
+//  GemSearch
+//
+//  Created by Abe on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct GemSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
